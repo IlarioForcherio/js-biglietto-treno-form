3b8ec6a6783314954664c93010aca99a59a79a08
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-20 20:31:47 +0200

    start project + add

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+/*Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65*/
+
+
+const inputNomeCognome = document.getElementById('nome-cognome');
+const inputKm = document.getElementById('km');
+let inputEta = document.getElementById('age');
+
+
+
+
+
+
+function Genera(){
+//Età
+    let valueEta = (inputEta.value);
+    console.log(valueEta);
+//Nome e cognome   
+    let valueNomeCognome = inputNomeCognome.value;
+    document.getElementById("nome-passeggero").innerHTML=` ${valueNomeCognome}`;
+//Kilometri   
+    let valueKm = parseInt(inputKm.value);
+    console.log(valueKm);
+
+
+
+    //prezzo biglietto
+    let prezzoTicket = valueKm * 0.21;
+    console.log(prezzoTicket);
+    
+    //sconti
+    let sconto20 = prezzoTicket * 0.2;
+    
+    let sconto40 = prezzoTicket / 100 * 40;
+    
+    //condizione 
+   
+    if (valueEta === 'minore18'){
+    prezzoTicket = prezzoTicket - sconto20;
+    
+    document.getElementById("ticket").innerHTML= `Il prezzo del biglietto è: ${prezzoTicket.toFixed(2)} Euro`;
+   
+    } else if (valueEta === 'maggiore65'){
+    prezzoTicket = prezzoTicket - sconto40;
+    
+    document.getElementById("ticket").innerHTML= `Il prezzo del biglietto è: ${prezzoTicket.toFixed(2)} Euro`;
+
+   }else (valueEta === 'altro');{
+    prezzoTicket = prezzoTicket;
+    document.getElementById("ticket").innerHTML= `Il prezzo del biglietto è: ${prezzoTicket.toFixed(2)} Euro`;
+}
+
+   
+ 
+
+}
